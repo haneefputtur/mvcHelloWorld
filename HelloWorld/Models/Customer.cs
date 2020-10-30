2bba514e69fe9ac2f06593a410d2c45f78c164a7
Haneef Puttur <cdsputtur@gmail.com>
2020-10-30 14:34:10 +0400

    Update

---
+++ HelloWorld/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int customer_id { get; set; }
+        [DisplayName("Customer Name")]
+        public string customer_name { get; set; }
+        [DisplayName("Customer Email")]
+        public string customer_email { get; set; }
+        [DisplayName("Customer Address")]
+        public string customer_address { get; set; }
+    }
+}
